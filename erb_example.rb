19eff907e7028773bc6496ca6a0aa8bb099ef485
Laurie Gloge <laurie@friendlyroad.com>
2015-02-26 17:13:36 -0700

    My first homework assignment

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,22 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = 'laurie'
+erb_string = "<h2><%= name.upcase %></h2>"
+
+template = ERB.new(erb_string)
+puts template.result(binding)
+# => <h2>RICHARD</h2>
+
+pets = ['Taz', 'Sunny', 'Panther', 'Timber', 'Pepper', 'Cocoa', 'Teddy', 'Salty', 'Twerp']
+
+		pets.each do |pet| 
+			puts "I took in #{pet}"
+
+		end 
+
+	puts "That is #{pets.size} furry friends!"
+
+
+
